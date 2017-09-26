bfc6a6c373042ac7178e2e27ed30ef0266b8e898
VBot2410 <vaibhav24.vb@gmail.com>
2017-09-25 20:19:47 -0400

    Implemented the Function compute & Constructor std::domain_error

--- app/PID.cpp
+++ app/PID.cpp
@@ -11,48 +11,56 @@
 
 /**
 
-* @brief Constructor used to initialize all the private members 
+ * @brief Constructor used to initialize all the private members
 
-* @param kp - is of type double used to initialize the private member Kp
+ * @param kp - is of type double used to initialize the private member Kp
 
-* @param kd - is of type double used to initialize the private member Kd
+ * @param kd - is of type double used to initialize the private member Kd
 
-* @param ki - is of type double used to initialize the private member Ki
+ * @param ki - is of type double used to initialize the private member Ki
 
-* @param t - is of type double used to initialize the private member dt
+ * @param t - is of type double used to initialize the private member dt
 
-* @returns nothing
-*/
+ * @returns nothing
+ */
 
 /* Throws an error if kp,kd,ki are less than zero*/
 /* Throws an error t is less than or equal to zero*/
 
-PID::PID(double kp, double kd, double ki, double t){
-
-  Kp=kp;Kd=kd;Ki=ki;dt=t;
-  error=0;
-  previousError=0;
-  errorSum=0;
-  errorRateOfChange=0;
-	
+PID::PID(double kp, double kd, double ki, double t) {
+  if (kp < 0 || kd < 0 || ki < 0 || t <= 0)
+    throw std::domain_error(
+        "Kp,Ki,Kd Cannot be Negative and t cannot be less than or equal to zero");
+  else {
+    Kp = kp;
+    Kd = kd;
+    Ki = ki;
+    dt = t;
+    error = 0;
+    previousError = 0;
+    errorSum = 0;
+    errorRateOfChange = 0;
+  }
 }
 
 /**
 
-* @brief computes controller output based on the values of Kp,Kd,Ki,dt
+ * @brief computes controller output based on the values of Kp,Kd,Ki,dt
 
-* @param setPoint - is of type double used to input the maximum velocity
+ * @param setPoint - is of type double used to input the maximum velocity
 
-* @param velocity - is of type double used to input 
+ * @param velocity - is of type double used to input
 
-* @returns the value of the controller output
+ * @returns the value of the controller output
 
-*/
+ */
 
 double PID::compute(double setPoint, double velocity) {
-
-return 0.94764;
-
+  error = setPoint - velocity;
+  errorSum = errorSum + (error * dt);
+  errorRateOfChange = (error - previousError) / dt;
+  previousError = error;
+  return ((Kp * error) + (Ki * errorSum) + (Kd * errorRateOfChange));
 }
 
 /* Destructor*/
